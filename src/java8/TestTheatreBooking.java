12c28db720c61a79205049cd050e803994999db1
Priyanka <priyanka.bolisetty@mail.npu.edu>
2016-08-27 19:59:11 -0700

    Test Theatre booking class

--- src/java8/TestTheatreBooking.java
+++ src/java8/TestTheatreBooking.java
@@ -1,10 +1,19 @@
 package java8;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class TestTheatreBooking {
 	public static void main(String[] args){
-		ThreatreBooking bookTheatre = new ThreatreBooking(LocalDate.of(2016, 8, 26));
+		int day;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("*****Book a Theatre*******");
+		System.out.print("Enter a date: ");
+		day = scan.nextInt();
+		
+		ThreatreBooking bookTheatre = new ThreatreBooking(LocalDate.of(2016, 8, day));
 		bookTheatre.getNearestAvailableDate();
 	}
 }
