3dbf7ed7795fb2f59dad21faac9d50d1e3512a3b
PriyankaBolisetty <PriyankaBolisetty@users.noreply.github.com>
2016-09-06 20:59:03 -0700

    Add files via upload

---
+++ src/Spring/OrderProcessor-Automatic-Beans-Discovery/main/client/OrderApplication.java
@@ -0,0 +1,43 @@
+package com.npu.orders.client;
+
+import com.npu.orders.domain.*;
+import com.npu.orders.services.InventoryService;
+import com.npu.orders.services.OrderProcessor;
+
+import java.util.ArrayList;
+
+import org.springframework.context.*;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class OrderApplication {
+	public static void main(String args[]) {
+		ApplicationContext container = new ClassPathXmlApplicationContext("app-context.xml");
+		Order order;
+		OrderProcessor orderProc = (OrderProcessor)container.getBean("orderProc");
+		InventoryService inventService = (InventoryService)container.getBean("inventoryServiceImpl");
+		
+		inventService.printCurrentInventory();
+		
+		Product product1 = new Product("iPhone", 600.00);
+		OrderItem item1 = new OrderItem(product1, 2);
+		
+		Product product2 = new Product("Shoes", 45.50);
+		OrderItem item2 = new OrderItem(product2, 4);
+		
+		order = new Order("GSX-56789", "AZ");
+		
+		order.addItem(item1);
+		order.addItem(item2);
+		order.addItem(item1);
+		order.addItem(item1);
+		order.removeProduct(product1);
+		
+		ArrayList<OrderItem> orderedItems = order.getLstOrderItems();
+		for(int i = 0; i < orderedItems.size(); i++){
+			System.out.println(orderedItems.get(i).getProduct().getName() + ": " + orderedItems.get(i).getQuantity());
+		}
+		
+		orderProc.newOrder(order);
+		inventService.printCurrentInventory();
+	}
+}
